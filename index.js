1190a3986313e6aa21dfa18d2cc1bc28819c575d
William Zembricki <wzembricki@gmail.com>
2021-03-15 12:08:55 -0400

    commit tree fd6cbee4a901a01734ebc48a2a2522dae3cbb046

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
+let greeting;
+function scuberGreetingForFeet(feet){
+  if (feet <= 400){
+  greeting = `This one is on me!`
+  return greeting 
+}
+  else if (feet > 2000 && feet <= 2499){
+  greeting =  'I will gladly take your thirty bucks.'
+  return greeting
+}
+  else if (feet > 2500){
+greeting = `No can do.`
+return greeting 
+  }
+  // Write your   code here!
+}
+
+function ternaryCheckCity(city){
+  if (city === `NYC`){
+    greeting = `Ok, sounds good.`
+    return greeting 
+  }
+  else {
+    greeting =`No go.`
+    return greeting 
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case "generous":
+      greeting = "Thank you so much."
+      return greeting;
+    case "not as generous":
+      greeting = `Thank you.`
+      return greeting;
+    case "thanks for everything": 
+      greeting = `Bye.`
+      return greeting;    
+  }
+ // Write your code here!
 }
